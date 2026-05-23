0c849317b9224e2027e7482e41b2572bc56dca2f
pradeepbanoth <banothpradeep50@gmail.com>
2026-05-23 17:06:48 +0530

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,6 +50,15 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} h-full antialiased`}>
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-EK9BGF7ZMR" />
+<script dangerouslySetInnerHTML={{
+  __html: `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-EK9BGF7ZMR');
+  `
+}} />
         <link rel="preconnect" href="https://api.fontshare.com" />
         <link
           href="https://api.fontshare.com/v2/css?f[]=clash-display@400,500,600,700&display=swap"
