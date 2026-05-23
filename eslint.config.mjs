9443f14a0fc78831980ed77fb7f9a0668de7bd69
pradeepbanoth <banothpradeep50@gmail.com>
2026-05-23 15:05:55 +0530

    Fix ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,14 +5,19 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@next/next/no-html-link-for-pages": "off",
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
